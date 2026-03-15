3c95fc5236f4ca517db7080c4298533107a8bf1a
Yaroslav Myhel <ist23-ya.myhel@nubip.edu.ua>
2026-03-15 21:21:41 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,19 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CarController;
 use App\Http\Controllers\Admin\CarController as AdminCarController;
 
-// 1. Публічні маршрути
+//Публічні маршрути
 Route::get('/', [CarController::class, 'index'])->name('cars.index');
 Route::get('/cars/{car}', [CarController::class, 'show'])->name('cars.show');
 
-// 2. Маршрут, який шукає Breeze (Виправляє помилку Route [dashboard] not defined)
 Route::get('/dashboard', function () {
-    // Якщо користувач адмін — в адмінку, якщо ні — в каталог
+
     if (auth()->user()->is_admin) {
         return redirect()->route('admin.dashboard');
     }
     return redirect()->route('cars.index');
 })->middleware(['auth'])->name('dashboard');
 
-// 3. Група адмін-панелі (закрита через middleware auth)
+//Група адмін-панелі
 Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
     Route::get('/', [AdminCarController::class, 'dashboard'])->name('dashboard');
     Route::get('/cars', [AdminCarController::class, 'index'])->name('cars.index');
